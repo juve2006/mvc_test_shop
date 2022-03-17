4485b23a550baadf7cf761059f7df0a66f04d809
Evgen Sikora <arnika200@gmail.com>
2022-03-17 16:02:09 +0200

    login viev add, customer controoller add

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Controllers;
 
 use Core\Controller;
-use Core\View;
+
 
 /**
  * Class ProductController
  */
 class CustomerController extends Controller
 {
+    private int $invalid_password;
 
     /**
      * Customer index action that shows customer list
@@ -57,5 +58,44 @@ class CustomerController extends Controller
         $this->set('customer', $customer);
 
         $this->renderLayout();
+    }
+    
+    public function LoginAction()
+    {
+        $this->set('title', "Вхід");
+        if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
+            $email = filter_input(INPUT_POST, 'email');
+            $password = password_hash(filter_input(INPUT_POST, 'password'), PASSWORD_DEFAULT);
+            $params = [
+                'email' => $email,
+                'password' => $password
+            ];
+            $customer = $this->getModel('customer')->initCollection()
+                ->filter($params)
+                ->getCollection()
+                ->selectFirst();
+            if (!empty($customer)) {
+                $_SESSION['id'] = $customer['customer_id'];
+                $this->redirect('/index/index');
+            } else {
+                $this->invalid_password = 1;
+            }
+        }
+        $this->renderLayout();
+    }
+
+    public function LogoutAction()
+    {
+        $this->clearSession();
+        $this->redirect('/index/index');
+    }
+
+    private function clearSession(): void
+    {
+        $_SESSION = [];
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), "", time() - 3600, "/");
+        }
+        session_destroy();
     }
 	}
--- app/views/customer/login.php
+++ app/views/customer/login.php
@@ -1,1 +1,19 @@
+<h1> Вхід </h1>
+<form method = "POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+   Введіть e-mail:
+    <label>
+        <input  type ='email' name ='email'>
+    </label> <br>
+    Введіть пароль:
+    <label>
+        <input type ='password' name ='password'>
+    </label> <br>
+
+    <input type = 'submit' name = 'login' value="Ввести">
+</form>
+
 <?php
+
+var_dump($_SESSION);
+
+var_dump($_POST);
